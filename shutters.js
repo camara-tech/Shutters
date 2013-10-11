473bcc121f376fbeb3ca2ac598bbeabb65377460
Jonathan Camara <italy_portugal@yahoo.com>
2013-10-11 10:17:27 -0500

    merged search designs
    brought together the two different search designs into one page and put
    in a toggle switch to move between the two.

---
+++ shutters.js
@@ -0,0 +1,7 @@
+//toggle the design for sorting search results
+$(document).ready(function() {
+	$("#toggle").click(function(){
+		$("#properties").toggle();
+		$("#content-header").toggle();
+	});
+});
