675363b3100294ac10e320f9c0029b55c81c2017
Jonathan Camara <italy_portugal@yahoo.com>
2013-11-01 17:46:42 -0500

    improved CSS and worked on functions in JS file
    index.html: Added some more structure to make CSS easier
    shutters.js: filled out the functions for retrieving sorted data
    searchresults.html: rewrote the file to include the final result run the
    js in index.html
    shutters.css: started separating the grid view from the list view,
    general improvements in presentation.

--- js/shutters.js
+++ js/shutters.js
@@ -4,11 +4,11 @@ function ShutterItem(shutter) {
 
 	self.name = shutter.ProductName;
 	self.price = "$" + shutter.Price;
-	self.rating = shutter.AvgRating;
-	self.image = "<img src='" + shutter.image + "' alt='" + self.name + "' />"
+	self.rating = shutter.AvgRating + " out of 10";
+	self.image = "<img src='" + shutter.image + "' alt='" + self.name + "' height='140' width='150'	/>"
 	
 	if (shutter.Featured) {
-	self.featured = "featured"
+	self.featured = "<img src='http://www.blinds.com/skin07/images/category/featured_graphic.png' alt=''/>"
 	}
 	else {
 	self.featured = ""
@@ -18,6 +18,9 @@ function ShutterItem(shutter) {
 //the shutter view model
 function ShuttersViewModel() {
 	var self = this;
+	
+	var gridview = false;
+	var listview = true;
 	
 	//the array of shutters that we are messing with
 	self.shutters = ko.observableArray();
@@ -70,28 +73,81 @@ function ShuttersViewModel() {
 	console.log(self.shutters());
 	//what happens when the sort by name link is clicked
 	self.sortByName = function () {
+		// switch to loading screen
+		$("#content").fadeOut();
+		$("#loading").fadeIn();
+		//clear an old data
+		self.shutters([]);
+		// get the sorted results
+		$.getJSON("js/alphabeticalsearchresults.json",function(data){
+			for (var i=0;i<data.length;i++){
+				// refill data
+				self.shutters.push(newShutterItem(data[i]));
+			}
+		});
+		
+		//remove loading screen and show all the content
+		$("#loading").fadeOut();
+		$("#content").slideDown();
 	}
+	
 	
 	//what happens when the sort by price link is clicked
 	self.sortByPrice = function () {
+		// switch to loading screen
+		$("#content").fadeOut();
+		$("#loading").fadeIn();
+		//clear an old data
+		self.shutters([]);
+		// get the sorted results
+		$.getJSON("js/pricesortedsearchresults.json",function(data){
+			for (var i=0;i<data.length;i++){
+				// refill data
+				self.shutters.push(newShutterItem(data[i]));
+			}
+		});
+		
+		//remove loading screen and show all the content
+		$("#loading").fadeOut();
+		$("#content").slideDown();
 	}
 	
 	//what happens when the grid view link is clicked
 	self.showGridView = function () {
+		//change the html to use the css that renders a grid
+		if (listview == true) {
+			listview = false;
+			// $(#content).addClass("grid");
+			gridview = true;
+			
+			
+		}
 	}
 	
 	//what happens when the List view link is clicked
 	self.showListView = function () {
+		//change the html to use the css that renders a list
+		if (gridview == true) {
+			gridview = false;
+			// $(#content).removeClass("grid");
+			listview = true;
+		}
 	}
 	
-	//what happens when showing five items
-	self.showFiveItems = function() {}
+	//what happens when showing four items
+	self.showFourItems = function() {
+		//show only four items in the list/grid
+		
+	}
 	
-	//what happens when showing ten items
-	self.showTenItems = function() {}
+	//what happens when showing eight items
+	self.showEightItems = function() {
+		//show only eight Items in the list/grid
+	}
 	
 	//what happens when showing infinite items
 	self.showInfiniteItems = function() {}
+		//load some items, load more items if the page scrolls down.
 }
 
 // setup knockout
